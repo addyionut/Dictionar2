6313d391adcb33416af1c508a2984b8ed651f94a
Adrian Ionut Zaharia <addyionut@gmail.com>
2022-09-15 16:28:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,34 @@ const wordInput = document.getElementById("wInput");
 const wordAdded = document.getElementById("inputValidation");
 const wordSearch = document.getElementById("wSearch");
 const wordFound = document.getElementById("searchValidation")
-const wordDictionary = new Set();
+wordInput.focus();
+const wordsDictionary = new Set();
 
 function saveWord() {
+	wordFound.innerHTML = "";
 	let currentWord = wordInput.value;
-	wordDictionary.add(currentWord);
-	console.log(wordDictionary);
+	wordsDictionary.add(currentWord);
 	wordAdded.innerHTML = `The word "${currentWord}" was added in the dictionary`;
-	wordInput.value = "";
-	wordInput.focus();
+	addWordSetUp();
 }
 
 function searchWord() {
+	wordAdded.innerHTML = "";
 	let searchWord = wordSearch.value;
-	if (wordDictionary.has(searchWord)) {
+	if (wordsDictionary.has(searchWord)) {
 		wordFound.innerHTML = `The word "${searchWord}" exists in the dictionary`;
 	} else {
 		wordFound.innerHTML = `The word "${searchWord}" is not exists in the dictionary`;
 	}
+	searchWordSetUp();
 }
 
+function addWordSetUp() {
+	wordInput.value = "";
+	wordInput.focus();
+}
+
+function searchWordSetUp() {
+	wordSearch.value = "";
+	wordSearch.focus();
+}
